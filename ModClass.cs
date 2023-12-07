23cfe8016f51c359a8ec03475a98522a623a4317
JupiterHoly <96080685+JupiterHoly@users.noreply.github.com>
2023-12-07 09:19:36 -0600

    Initial commit

---
+++ ModClass.cs
@@ -0,0 +1,16 @@
+﻿using NeoModLoader.api;
+
+namespace CHANGEME
+{
+    public class ModClass : BasicMod<ModClass>
+    {
+        protected override void OnModLoad()
+        {
+            // Load your mod here
+            // 加载你的mod内容
+            
+            // LogInfo(GetConfig()["Default"]["WhatToSay"].TextVal); // Call this only then you confirm it is a text config item
+            LogInfo(GetConfig()["Default"]["WhatToSay"].GetValue() as string);
+        }
+    }
+}
